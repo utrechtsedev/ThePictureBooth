d4b61d527a6091959d7c845aef928cad7706d3ae
Utrechtsedev <aichou@innovatieweb.nl>
2025-04-08 23:55:41 +0200

    enabled ssl

--- src/lib/server/models/database.js
+++ src/lib/server/models/database.js
@@ -2,9 +2,10 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv'; dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST, 
+  host: process.env.DB_HOST,
   dialect: "mariadb",
   logging: false,
+  ssl: true
 });
 
 
