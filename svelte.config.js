dcd75305627d68b8e33a396071b636e9b6e2348f
Utrechtsedev <aichou@innovatieweb.nl>
2025-04-08 23:30:23 +0200

    fixed adapter

--- svelte.config.js
+++ svelte.config.js
@@ -2,34 +2,29 @@ import adapter from '@sveltejs/adapter-node';
 // import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-        warningFilter: (warning) => {
-            const ignore = [
-                'a11y_media_has_caption',
-                'a11y_no_redundant_roles',
-                'a11y_consider_explicit_label',
-                'a11y_no_noninteractive_tabindex',
-                'a11y_click_events_have_key_events',
-                'a11y_no_static_element_interactions',
-                'a11y_no_noninteractive_element_interactions',
-            ]
-            return !ignore.includes(warning.code)
-        },
+  compilerOptions: {
+    warningFilter: (warning) => {
+      const ignore = [
+        'a11y_media_has_caption',
+        'a11y_no_redundant_roles',
+        'a11y_consider_explicit_label',
+        'a11y_no_noninteractive_tabindex',
+        'a11y_click_events_have_key_events',
+        'a11y_no_static_element_interactions',
+        'a11y_no_noninteractive_element_interactions',
+      ]
+      return !ignore.includes(warning.code)
     },
-	kit: {
-		csrf: {
-            checkOrigin: true,
-		},
-		alias: {
-			"@/*": "/src/lib",
-		},
-		adapter: adapter({
-			out: 'build', // Output directory (standaard: 'build')
-			precompress: true, // Schakel compressie in (optioneel)
-			envPrefix: 'APP_' // Aangepaste environment variable prefix (optioneel)
-
-		  })
-	},
+  },
+  kit: {
+    csrf: {
+      checkOrigin: true,
+    },
+    alias: {
+      "@/*": "/src/lib",
+    },
+    adapter: adapter()
+  },
 };
 
 export default config;
