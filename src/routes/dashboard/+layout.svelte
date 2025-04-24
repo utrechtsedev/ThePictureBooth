85414d9df3357169fc0f2ed70f40eff5ee607cbb
Utrechtsedev <moughit53@gmail.com>
2025-04-24 23:37:57 +0200

    fixed profile broken link

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -655,7 +655,7 @@
               Instellingen
             </a>
             <a
-              href="/dashboard/profile"
+              href="#"
               class="flex items-center px-4 py-2 text-sm {prefersDarkMode
                 ? 'text-gray-300 hover:bg-gray-700'
                 : 'text-gray-700 hover:bg-gray-100'}"
